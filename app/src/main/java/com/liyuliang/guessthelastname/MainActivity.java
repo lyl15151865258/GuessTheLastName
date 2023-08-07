8a8b87fb21869542845e511b321b2a3d4a0a92cd
LiYuliang <1692531768@qq.com>
2023-08-07 19:23:37 +0800

    完善姓氏

--- app/src/main/java/com/liyuliang/guessthelastname/MainActivity.java
+++ app/src/main/java/com/liyuliang/guessthelastname/MainActivity.java
@@ -1,23 +1,35 @@
 package com.liyuliang.guessthelastname;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.PagerSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private Context mContext;
     private RecyclerView recyclerView;
     private CardAdapter cardAdapter;
+    private AppCompatSpinner spLastName;
+
+    private List<List<String>> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mContext = this;
         recyclerView = findViewById(R.id.recyclerView);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
@@ -28,8 +40,30 @@ public class MainActivity extends AppCompatActivity {
 
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, layoutManager.getOrientation());
         recyclerView.addItemDecoration(dividerItemDecoration);
+        data = Constant.getLastNameList(Constant.LAST_NAME1);
+        cardAdapter = new CardAdapter(mContext, data);
 
-        cardAdapter = new CardAdapter(this, Constant.getLastNameList(Constant.LAST_NAME1));
+        spLastName = findViewById(R.id.spLastName);
+        spLastName.setSelection(0);
+
+        List<String[]> names = new ArrayList<>();
+        names.add(Constant.LAST_NAME1);
+        names.add(Constant.LAST_NAME2);
+        names.add(Constant.LAST_NAME3);
+        names.add(Constant.LAST_NAME4);
+        spLastName.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                data.clear();
+                data.addAll(Constant.getLastNameList(names.get(position)));
+                cardAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         recyclerView.setAdapter(cardAdapter);
     }
 }
